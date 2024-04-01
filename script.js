b89b97aab4be3dd605485a33f15885a517ca3862
“sumeetjain2002” <sumeetjain.com01@gmail.com>
2024-04-01 17:41:01 +0530

    updating api to fetch 5 day data

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const apiId = "69b0b99058c035044f842679ebf46825";
 const url = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const url1 = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 
 async function checkWeather(city) {
-  const response = await fetch(url + `&q=${city}` + `&appid=${apiId}`);
+  const response = await fetch(
+    url1 + `&q=${city}` + `&cnt=5` + `&appid=${apiId}`
+  );
   var data = await response.json();
   console.log(data);
-  document.querySelector(".city").innerText = data.name;
-  document.querySelector(".temp").innerText = data.main.temp + " °С";
-  document.querySelector(".humidity").innerText = data.main.humidity + " %";
-  document.querySelector(".wind").innerText = data.wind.speed + " Km/h";
+  document.querySelector(".city").innerText = data.city.name;
+  document.querySelector(".temp").innerText = data.list[0].main.temp + " °С";
+  document.querySelector(".humidity").innerText =
+    data.list[0].main.humidity + " %";
+  document.querySelector(".wind").innerText = data.list[0].wind.speed + " Km/h";
   let icon = document.querySelector(".weather-icon");
-  console.log(data.weather[0].id);
-  if (data.weather[0].id >= 200 && data.weather[0].id < 300) {
+  let id=data.list[0].weather[0].id;
+  if (id >= 200 && id < 300) {
     icon.src = "/images/thunderstorm.png";
-  } else if (data.weather[0].id >= 300 && data.weather[0].id < 330) {
+  } else if (id >= 300 && id < 330) {
     icon.src = "/images/drizzle.png";
-  } else if (data.weather[0].id >= 500 && data.weather[0].id < 540) {
+  } else if (id >= 500 && id < 540) {
     icon.src = "/images/rain.png";
-  } else if (data.weather[0].id >= 600 && data.weather[0].id < 630) {
+  } else if (id >= 600 && id < 630) {
     icon.src = "/images/snow.png";
-  } else if (data.weather[0].id >= 700 && data.weather[0].id < 800) {
+  } else if (id >= 700 && id < 800) {
     icon.src = "/images/mist.png";
-  } else if (data.weather[0].id == 800) {
+  } else if (id == 800) {
     icon.src = "/images/clear.png";
   } else {
     icon.src = "/images/clouds.png";
   }
 }
+
+
 let city = document.querySelector(".inp");
 // added enter functiionality
 city.addEventListener("keypress", (event) => {
@@ -34,7 +40,7 @@ city.addEventListener("keypress", (event) => {
     checkWeather(city.value);
     city.value = "";
   }
-});  
+});
 
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
