d9d087e6d3e458afe492e2e020c8ec30723f86c4
“sumeetjain2002” <sumeetjain.com01@gmail.com>
2024-04-01 18:41:33 +0530

    updating + icons updating now

--- script.js
+++ script.js
@@ -14,23 +14,45 @@ async function checkWeather(city) {
     data.list[0].main.humidity + " %";
   document.querySelector(".wind").innerText = data.list[0].wind.speed + " Km/h";
   let icon = document.querySelector(".weather-icon");
-  let id=data.list[0].weather[0].id;
-  if (id >= 200 && id < 300) {
-    icon.src = "/images/thunderstorm.png";
-  } else if (id >= 300 && id < 330) {
-    icon.src = "/images/drizzle.png";
-  } else if (id >= 500 && id < 540) {
-    icon.src = "/images/rain.png";
-  } else if (id >= 600 && id < 630) {
-    icon.src = "/images/snow.png";
-  } else if (id >= 700 && id < 800) {
-    icon.src = "/images/mist.png";
-  } else if (id == 800) {
-    icon.src = "/images/clear.png";
-  } else {
-    icon.src = "/images/clouds.png";
-  }
+  let ic1 = document.querySelector(".weather1");
+  let ic2 = document.querySelector(".weather2");
+  let ic3 = document.querySelector(".weather3");
+  let ic4 = document.querySelector(".weather4");
+
+  let id = data.list[0].weather[0].id;
+  icon.src=iconSetter(id);
+ 
+
+  id = data.list[1].weather[0].id;
+  ic1.src=iconSetter(id);
+  id = data.list[2].weather[0].id;
+  ic2.src=iconSetter(id);
+  id = data.list[3].weather[0].id;
+  ic3.src=iconSetter(id);
+  id = data.list[4].weather[0].id;
+  ic4.src=iconSetter(id);
+
+  
+  
+  temp1.innerText = data.list[1].main.temp+" °С";
+  temp2.innerText = data.list[2].main.temp+" °С";
+  temp3.innerText = data.list[3].main.temp+" °С";
+  temp4.innerText = data.list[4].main.temp+" °С";
+  
+ 
+
 }
+
+let temp1 = document.querySelector(".w1");
+let temp2 = document.querySelector(".w2");
+let temp3 = document.querySelector(".w3");
+let temp4 = document.querySelector(".w4");
+
+let i1 = document.querySelector(".weather1");
+let i2 = document.querySelector(".weather2");
+let i3 = document.querySelector(".weather3");
+let i4 = document.querySelector(".weather4");
+
 
 
 let city = document.querySelector(".inp");
@@ -41,6 +63,25 @@ city.addEventListener("keypress", (event) => {
     city.value = "";
   }
 });
+
+function iconSetter(id){
+  if (id >= 200 && id < 300) {
+    src = "/images/thunderstorm.png";
+  } else if (id >= 300 && id < 330) {
+    src = "/images/drizzle.png";
+  } else if (id >= 500 && id < 540) {
+    src = "/images/rain.png";
+  } else if (id >= 600 && id < 630) {
+    src = "/images/snow.png";
+  } else if (id >= 700 && id < 800) {
+   src = "/images/mist.png";
+  } else if (id == 800) {
+    src = "/images/clear.png";
+  } else {
+    src = "/images/clouds.png";
+  }
+  return src;
+}
 
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
