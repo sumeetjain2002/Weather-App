c4129a0a92d9b4212292f76322b054dcf7a95387
“sumeetjain2002” <sumeetjain.com01@gmail.com>
2024-04-22 23:02:04 +0530

    Using .env file

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const axios=require("axios");
+require('dotenv').config();     // to include .env file
 const PORT = 4000;
-const apiId = "69b0b99058c035044f842679ebf46825";
-const url = "https://api.openweathermap.org/data/2.5/weather?units=metric";
-const url1 = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
+
+let url=process.env.url1;
+let key=process.env.apiId;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,7 +19,7 @@ app.post("/", async (req, res) => {
   let { city } = req.body;
   console.log(city);
 
-     let a= await axios(url1 + `&q=${city}` + `&cnt=5` + `&appid=${apiId}`)
+  let a= await axios(url + `&q=${city}` + `&cnt=5` + `&appid=${key}`)
     a=JSON.stringify(a.data);
     console.log(a);
     res.send(a);
